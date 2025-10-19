82fc3358beda876864888bc7b1d1540bb917b718
Andres Velez <af.velezv1@uniandes.edu.co>
2025-10-19 10:31:37 +0200

    Add Gunicorn configuration and update Elastic Beanstalk settings

---
+++ blacklist-service/gunicorn.conf.py
@@ -0,0 +1,14 @@
+import os
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
+workers = 4
+worker_class = "gthread"
+worker_connections = 1000
+timeout = 30
+keepalive = 10
+max_requests = 1000
+max_requests_jitter = 50
+preload_app = True
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
